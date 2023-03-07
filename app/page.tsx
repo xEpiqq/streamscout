7f7946e4cb767e312a39680bd0d92d005fd54af0
xEpiqq <49432366+xEpiqq@users.noreply.github.com>
2023-03-07 03:27:36 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,59 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client'
+import React from 'react'
+import s from './page.module.scss'
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const current_movies = []
 
-export default function Home() {
+function page() {
+
+  const [addMovie, setAddMovie] = useState("")
+  const [rerenderHack, setRerenderHack] = useState(false)
+
+  function addMovieToList() {
+      const addMovieWithoutSpaces = addMovie.replace(/\s/g, '')
+      if (addMovieWithoutSpaces !== "") {
+        current_movies.push(addMovie)
+        setAddMovie("")
+      }
+  }
+
+  function submitList() {
+    current_movies.length = 0;
+    console.log("submitted")
+    setRerenderHack(!rerenderHack)
+  }
+
+  function deleteItem(movie) {
+    console.log(movie)
+    current_movies.splice(current_movies.indexOf(movie), 1)    
+    console.log("deleted")
+    setRerenderHack(!rerenderHack)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={s.home}>
+      <div className={s.navbar} >
+        <img src='/ferrett.png' />
+        <h2>StreamScout</h2>
+      </div>
+      <div className={s.content} >
+        <div className={s.searchbox_container}>
+          <input className={s.searchbox} type='text' value={addMovie} onChange={(e) => setAddMovie(e.target.value)} 
+          onKeyDown={(e) => { if (e.keyCode === 13) { addMovieToList()}}} placeholder='Add a movie to the list' />
+          <button className={s.addbutton} type='submit' onClick={() => addMovieToList()}>+</button>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className={s.searchresults}>
+            {current_movies.map((movie, index) => (
+              <div className={s.movie_element} key={index}>{movie}<img className={s.tiny_x} src='/tiny_x.svg' onClick={() => deleteItem(movie)}/></div>
+            ))}
         </div>
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <button className={s.submitbutton} type='submit' onClick={() => submitList()}>submit</button>
+    </div>
   )
 }
+
+export default page
